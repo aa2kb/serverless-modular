9e9cdedf2b0e5028e213895cc98680fd82a92dde
Amin <amin@sudofy.com>
2018-08-03 12:37:52 +0500

    create functions.yml file from command

--- src/commands/build/build.controller.js
+++ src/commands/build/build.controller.js
@@ -4,8 +4,41 @@ const format = require('string-template');
 const utils = require('../../utils');
 
 class buildClass {
-  createFunctionsYml() {
-    console.log(this.options);
+  async createFunctionsYml() {
+    const feature = this.options.feature;
+    const srcPath = `${this.cwd}/src`;
+    const mainFunctionsPath = `${this.cwd}/functions.yml`;
+    let featureFunctions = [];
+    if (feature) {
+      featureFunctions = [{
+        path: `${srcPath}/${feature}/${feature}-functions.yml`,
+        name: feature
+      }];
+    } else {
+      featureFunctions = utils.getFeaturePath(srcPath).map((f) => {
+        const fSplit = f.split('/');
+        const fName = fSplit[fSplit.length - 1];
+        return {
+          path: `${f}/${fName}-functions.yml`,
+          name: fName
+        };
+      });
+    }
+
+    const mainFunctions = {};
+    for (const f of featureFunctions) {
+      const functionYml = await utils.ymltoJson(f.path);
+      for (const i in functionYml.functions) {
+        const currentFunction = functionYml.functions[i];
+        for (const j in currentFunction.events) {
+          const currentpath = currentFunction.events[j].http.path;
+          currentFunction.events[j].http.path = `${functionYml.basepath}/${currentpath}`;
+        }
+        mainFunctions[`${f.name}-${i}`] = currentFunction;
+      }
+    }
+    // console.log(mainFunctions);
+    fsPath.writeFileSync(mainFunctionsPath, utils.jsontoYml(mainFunctions));
   }
 }
 
