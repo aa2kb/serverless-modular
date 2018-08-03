9188765f64b576bf2c1a91b43def5bc771c3166a
Amin <amin@sudofy.com>
2018-08-03 12:38:13 +0500

    add utils functions for feautre reader

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const jsYaml = require('js-yaml');
 const jsonYml = require('json-to-pretty-yaml');
 const fs = require('fs');
+const path = require('path');
 
 async function ymltoJson(filename) {
   return jsYaml.safeLoad(await fs.readFileAsync(filename, 'utf8').then(data => data));
@@ -10,8 +11,16 @@ function jsontoYml(jsonData) {
   return jsonYml.stringify(jsonData);
 }
 
+function isDirectory(source) {
+  return fs.lstatSync(source).isDirectory();
+}
+
+function getFeaturePath(source) {
+  return fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+}
 
 module.exports = {
   ymltoJson,
-  jsontoYml
+  jsontoYml,
+  getFeaturePath
 };
